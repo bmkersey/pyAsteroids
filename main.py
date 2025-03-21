a2406fb480deb8402290ca70e2eb85e615190183
Brendan <bmkersey@gmail.com>
2025-03-21 12:45:12 -0400

    created asteroids and an asteroidfield, asteroids now spawn and move across the screen!

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
   print("Starting Asteroids!")
@@ -11,16 +13,26 @@ def main():
   clock = pygame.time.Clock()
   dt = 0
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  updateable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+  asteroids = pygame.sprite.Group()
+  Player.containers = (updateable, drawable)
+  Asteroid.containers = (asteroids, updateable, drawable)
+  AsteroidField.containers = (updateable)
+  field = AsteroidField()
   player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
   
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
+      
     screen.fill(BLACK)
-    player.draw(screen)
-    pygame.display.flip()
     dt = clock.tick(60) / 1000
+    updateable.update(dt)
+    for d in drawable:
+      d.draw(screen)
+    pygame.display.flip()
 
 
 if __name__ == "__main__":
